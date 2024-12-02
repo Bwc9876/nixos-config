aa95416e1a6d47fa2445b7b6ef4ec7cc79c723b5
Ben C <bwc9876@gmail.com>
2024-12-01 21:46:48 -0500

    Fix screensharing

--- systems/black-mesa.nix
+++ systems/black-mesa.nix
@@ -9,7 +9,7 @@
 
     includeBaseMods = true;
 
-    roles = ["latest-linux" "ssh" "dev" "secureboot" "mc-server"];
+    roles = ["latest-linux" "ssh" "fun" "dev" "secureboot" "mc-server"];
 
     extraModules = [
       ({
