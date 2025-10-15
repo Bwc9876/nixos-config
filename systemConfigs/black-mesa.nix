0017d5c94e03e741b1bd1dec8819b47f2fd2999c
Ben C <bwc9876@gmail.com>
2025-10-14 21:13:45 -0400

    Change over black-mesa WOL

--- systemConfigs/black-mesa.nix
+++ systemConfigs/black-mesa.nix
@@ -2,101 +2,139 @@
   outputs,
   inputs,
   ...
-}: {
+}:
+{
   system = "x86_64-linux";
-  specialArgs.inputs = inputs // inputs.spoon.inputs // {inherit (inputs) self;};
+  specialArgs.inputs = inputs // inputs.spoon.inputs // { inherit (inputs) self; };
 
   modules = [
     inputs.spoon.nixosModules.black-mesa
-    (outputs.lib.applyRoles ["base" "latest-linux" "networking" "ssh" "graphics" "games" "fun" "dev" "normalboot" "mc-server"])
+    (outputs.lib.applyRoles [
+      "base"
+      "latest-linux"
+      "networking"
+      "ssh"
+      "graphics"
+      "games"
+      "fun"
+      "dev"
+      "normalboot"
+      "mc-server"
+    ])
     {
-      imports = [inputs.bingus.nixosModules.default];
-      nixpkgs.overlays = [inputs.bingus.overlays.default];
+      systemd.network.links."79-eth-wol" = {
+        matchConfig = {
+          Type = "ether";
+          Driver = "!veth";
+          Virtualization = "false";
+        };
+        linkConfig = {
+          WakeOnLan = "magic";
+          NamePolicy = "keep kernel database onboard slot path";
+          AlternativeNamesPolicy = "database onboard slot path mac";
+          MACAddressPolicy = "persistent";
+        };
+      };
+    }
+    {
+      imports = [ inputs.bingus.nixosModules.default ];
+      nixpkgs.overlays = [ inputs.bingus.overlays.default ];
 
       services.bingus-bot = {
         enable = true;
-        replyChannels = [1295447496948191262 1295245646542143489];
+        replyChannels = [
+          1295447496948191262
+          1295245646542143489
+        ];
       };
     }
-    ({
-      modulesPath,
-      lib,
-      config,
-      pkgs,
-      ...
-    }: {
-      imports = [(modulesPath + "/installer/scan/not-detected.nix")];
-      networking.hostName = "black-mesa";
-      system.stateVersion = "25.05";
+    (
+      {
+        modulesPath,
+        lib,
+        config,
+        pkgs,
+        ...
+      }:
+      {
+        imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+        networking.hostName = "black-mesa";
+        system.stateVersion = "25.05";
 
-      boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-      boot.initrd.kernelModules = ["amdgpu"];
-      boot.kernelModules = ["kvm-amd"];
-      boot.extraModulePackages = [];
+        boot.initrd.availableKernelModules = [
+          "nvme"
+          "xhci_pci"
+          "ahci"
+          "usbhid"
+          "usb_storage"
+          "sd_mod"
+        ];
+        boot.initrd.kernelModules = [ "amdgpu" ];
+        boot.kernelModules = [ "kvm-amd" ];
+        boot.extraModulePackages = [ ];
 
-      services.pulseaudio.enable = false;
+        services.pulseaudio.enable = false;
 
-      security.rtkit.enable = true; # Allows pipewire and friends to run realtime
+        security.rtkit.enable = true; # Allows pipewire and friends to run realtime
 
-      services.pipewire = {
-        enable = true;
-        pulse.enable = true;
-        alsa = {
+        services.pipewire = {
           enable = true;
-          support32Bit = true;
+          pulse.enable = true;
+          alsa = {
+            enable = true;
+            support32Bit = true;
+          };
         };
-      };
 
-      programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
-        localNetworkGameTransfers.openFirewall = true;
-      };
+        programs.steam = {
+          enable = true;
+          remotePlay.openFirewall = true;
+          dedicatedServer.openFirewall = true;
+          localNetworkGameTransfers.openFirewall = true;
+        };
 
-      programs.gamescope = {
-        enable = true;
-        # package = pkgs.gamescope.overrideAttrs (new: old: {
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "ValveSoftware";
-        #     repo = "gamescope";
-        #     rev = "186f3a3ed0ce8eb5f3a956d3916a3331ea4e3ab2";
-        #     fetchSubmodules = true;
-        #     hash = "sha256-zAzIi3syJYtbKjydp19d1OxZvMjXb+eO+mXT/mJPEuA=";
-        #   };
-        # });
-        capSysNice = true;
-      };
+        programs.gamescope = {
+          enable = true;
+          # package = pkgs.gamescope.overrideAttrs (new: old: {
+          #   src = pkgs.fetchFromGitHub {
+          #     owner = "ValveSoftware";
+          #     repo = "gamescope";
+          #     rev = "186f3a3ed0ce8eb5f3a956d3916a3331ea4e3ab2";
+          #     fetchSubmodules = true;
+          #     hash = "sha256-zAzIi3syJYtbKjydp19d1OxZvMjXb+eO+mXT/mJPEuA=";
+          #   };
+          # });
+          capSysNice = true;
+        };
 
-      fileSystems."/" = {
-        device = "/dev/nvme0n1p2";
-        fsType = "ext4";
-      };
+        fileSystems."/" = {
+          device = "/dev/nvme0n1p2";
+          fsType = "ext4";
+        };
 
-      fileSystems."/boot" = {
-        device = "/dev/nvme0n1p1";
-        fsType = "vfat";
-      };
+        fileSystems."/boot" = {
+          device = "/dev/nvme0n1p1";
+          fsType = "vfat";
+        };
 
-      fileSystems."/mnt/storage" = {
-        device = "/dev/sda1";
-        fsType = "btrfs";
-      };
+        fileSystems."/mnt/storage" = {
+          device = "/dev/sda1";
+          fsType = "btrfs";
+        };
 
-      swapDevices = [];
+        swapDevices = [ ];
 
-      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+        hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-      hardware.graphics.enable = true;
-      services.xserver.videoDrivers = ["amdgpu"];
+        hardware.graphics.enable = true;
+        services.xserver.videoDrivers = [ "amdgpu" ];
 
-      networking.interfaces.enp4s0.wakeOnLan.enable = true;
-
-      # services.nix-serve = {
-      #   enable = true;
-      #   secretKeyFile = "/etc/nix-serve-key";
-      #   openFirewall = true;
-      # };
-    })
+        # services.nix-serve = {
+        #   enable = true;
+        #   secretKeyFile = "/etc/nix-serve-key";
+        #   openFirewall = true;
+        # };
+      }
+    )
   ];
 }
