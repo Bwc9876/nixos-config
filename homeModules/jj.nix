9ee017771704975e3f58495649b7388900c2dff0
Ben C <ben@bwc9876.dev>
2026-02-22 09:10:52 -0500

    JJ ui config

--- homeModules/jj.nix
+++ homeModules/jj.nix
@@ -14,14 +14,45 @@
     programs.jujutsu = {
       enable = true;
       settings = {
-        ui.default-command = "log";
-        ui.pager = "less -FR";
+        ui = {
+          default-command = [
+            "log"
+            "--reversed"
+            "-n"
+            "15"
+          ];
+          pager = "less -FR";
+          editor = lib.mkIf config.cow.neovim.enable "nvim";
+          diff-editor = ":builtin";
+          merge-editor = "mergiraf";
+        };
+        git = {
+          private-commits = "description('private:*')";
+        };
+
+        template-aliases = {
+          "format_short_id(id)" = "id.shortest()";
+          "format_timestamp(timestamp)" = "timestamp.ago()";
+        };
         aliases = {
-          "push" = ["git" "push"];
-          "pull" = ["git" "fetch"];
-          "bsm" = ["bookmark" "set" "main"];
+          "push" = [
+            "git"
+            "push"
+          ];
+          "pull" = [
+            "git"
+            "fetch"
+          ];
+          "bsm" = [
+            "bookmark"
+            "set"
+            "main"
+          ];
           "bm" = ["bookmark"];
-          "d" = ["describe" "-m"];
+          "d" = [
+            "describe"
+            "-m"
+          ];
           "s" = ["show -s"];
           "ss" = ["show"];
           "n" = ["new"];
@@ -31,6 +62,7 @@
     };
 
     home.packages = with pkgs; [
+      less
       mergiraf
     ];
   });
