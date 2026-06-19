356fe9f3d192b6739470c7fc79c3f7740cb568f9
Ben C <ben@bwc9876.dev>
2026-06-19 10:16:40 -0400

    timber-hearth: fill in modules

--- nixosConfigurations/timber-hearth.nix
+++ nixosConfigurations/timber-hearth.nix
@@ -1,21 +1,23 @@
 {
-	inputs,
-	outputs,
-	...
+  inputs,
+  outputs,
+  ...
 }: {
-	system = "x86_64-linux";
+  system = "x86_64-linux";
 
-	modules = [
-		outputs.nixosModules.default
-		inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x13-intel
-		{
+  modules = [
+    outputs.nixosModules.default
+    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x13-intel
+    {
+      networking.hostName = "timber-hearth";
+
       home-manager.users.bean = {
         home.stateVersion = "26.11";
         xdg.userDirs.setSessionVariables = true;
         cow.dev.enable = false;
       };
 
-			system.stateVersion = "26.11";
+      system.stateVersion = "26.11";
 
       users.users = let
         secureRoot = "/nix/persist/secure";
@@ -39,12 +41,27 @@
         };
         lanzaboote.enable = true;
         imperm.enable = true;
+        role-laptop = {
+          enable = true;
+          fingerPrintSensor = true;
+        };
         disks = {
           enable = true;
           swap = true;
-					luks = true;
+          luks = true;
         };
       };
-		}
-	];
+
+      boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "nvme"
+        "usb_storage"
+        "sd_mod"
+        "sdhci_pci"
+      ];
+      boot.initrd.kernelModules = [];
+      boot.kernelModules = ["kvm-intel"];
+      boot.extraModulePackages = [];
+    }
+  ];
 }
